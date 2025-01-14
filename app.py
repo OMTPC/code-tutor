6d4e6a082161ff80ab1b7fb9983dffacf10a229c
OMTPC <orlandocaetanouk@gmail.com>
2025-01-14 12:40:07 +0000

    base done

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config["SECRET_KEY"] = "CodeTutor_Project"
 @app.route("/")
 def home():
     #current_year = datetime.datetime.now().year
-    return render_template("Homepage.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
