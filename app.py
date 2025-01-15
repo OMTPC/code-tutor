01bfd8a37db271a4c6646b0157211dd2eff9a123
OMTPC <orlandocaetanouk@gmail.com>
2025-01-15 15:32:06 +0000

    databased created

--- app.py
+++ app.py
@@ -2,28 +2,36 @@ from flask import Flask, render_template, redirect, url_for, flash, request
 from forms import LoginForm, RegisterForm
 import datetime
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = "CodeTutor_Project"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///codetutorDB.db'  # This will create a file with your Db name
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # To disable a feature we don't need
 
-# Set up the database URI (SQLite for development)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///codementor.db'  # You can change this for other databases
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable tracking modifications to save memory
-
-# Initialize the database instance
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    userid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(250), nullable=False)  # Store hashed password
+    registered_at = db.Column(db.DateTime, default=datetime)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
 
 
 @app.route("/")
 def home():
     current_year = datetime.datetime.now().year
     return render_template("index.html", year = current_year)
-
-# Create the database and tables if they don't exist
-with app.app_context():
-    db.create_all()  # This will create all tables defined in models
 
 
 @app.route("/login", methods=["GET", "POST"])
